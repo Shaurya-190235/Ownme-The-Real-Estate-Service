7197085392ae997081cbcd4e6d61d7e375cb9b7e
Shaurya <sahishaurya313@gmail.com>
2022-05-24 13:19:00 +0545

    Working on user registeration

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login', login, name='login'),  
     path('logout', logout, name='logout'),
     path('dashboard', dashboard, name='dashboard'),
+    path('register', register, name='register'),
     
 ]
