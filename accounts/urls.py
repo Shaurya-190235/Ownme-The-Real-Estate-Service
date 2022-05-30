5e4ef7e97a57d3027f44cc1943528f7f18c6579d
Ashmitathakur <asmee2056@gmail.com>
2022-05-30 20:22:13 +0545

    urls added for user profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,10 @@ from .views import (login, logout )
 
 urlpatterns = [
     path('login', login, name='login'),  
+    path('register', register, name='register'),
     path('logout', logout, name='logout'),
+    path('dashboard', dashboard, name='dashboard'),
+    path('profile/<int:pk>', login_required(
+         ProfileUpdateView.as_view()), name='profile')
     
 ]
