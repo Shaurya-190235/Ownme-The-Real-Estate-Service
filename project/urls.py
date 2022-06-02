c81dfa8365954a6b6fbf0c4405b1a50517668ca8
Shaurya <sahishaurya313@gmail.com>
2022-06-02 17:31:22 +0545

    Working on product listings

--- project/urls.py
+++ project/urls.py
@@ -31,11 +31,24 @@ handler500 = 'core.views.server_error'
 urlpatterns = i18n_patterns(
     path('i18n/', include('django_translation_flags.urls')),
     path('admin/', admin.site.urls),
+    path('admin/contact/', AdminContactView.as_view(), name='admin-contact'),
 
-   
+    path('', include('core.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/auth/password_reset_done.html'),
+         name='password-reset-done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(  # noqa
+        template_name="accounts/auth/password_reset_confirm.html"),
+         name='password-reset-confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/auth/password_reset_complete.html'),
+         name='password_reset_complete'),
+
+    path('listings/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
-   
+    path('listings/', include('documents.urls')),
 
 )
 
